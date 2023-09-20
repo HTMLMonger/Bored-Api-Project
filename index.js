d4053983b8b53831bb10763c3469361e53fcffb7
M Mack <leithpcman@gmail.com>
2023-09-20 07:42:44 +0100

    Added Functionality
    Two elements have been given function, one to go.

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 'use strict';
-const axiosRequest = require('axios')
 
-axiosRequest
-    .get(`http://www.boredapi.com/api/activity?type=${eventType}&participants=${numPart}`)
-    .then(response => {
-         return 
-    })
-    .catch(error => {
-        window.alert(`ERROR! ${error}`)
-        console.error(`ERROR! ${error}`)
-    })
+myForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+let people = document.getElementById(`numPeople`).value;
+let type = document.getElementById(`type`).value;
+  
 
-    
+function boredFetch() {
+    fetch(`http://www.boredapi.com/api/activity?type=${type}&participants${people}`,{
+      })
+    .then( response => response.json() )
+    .then( response => addDom(response.activity) )
+}
+
+function addDom(info){
+    document.getElementById(`answer`).innerHTML = JSON.stringify(info);
+}
+boredFetch(people,type);
+})
+
+
+
+
 
