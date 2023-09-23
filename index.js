5007a4b85f0e8f60500f0c6895993c023dc80236
M Mack <leithpcman@gmail.com>
2023-09-23 02:33:23 +0100

    Re adjusted scope to reduce number of options
    *Gives more detail about each activity
    *Removed the ability to choose participant number and price as API did not support this
    *Adjusted UI for Mobile and Desktop

--- index.js
+++ index.js
@@ -1,25 +1,61 @@
 'use strict';
 
-myForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-let people = document.getElementById(`numPeople`).value;
-let type = document.getElementById(`type`).value;
+// Assuming you have a form element with the id "myForm" in your HTML.
+const myForm = document.getElementById('myForm');
+const answer = document.getElementById('answer');
+const answer2 = document.getElementById('answer2');
+const answer3 = document.getElementById('answer3');
+
+myForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const type = document.getElementById('type').value;
   
 
-function boredFetch() {
-    fetch(`http://www.boredapi.com/api/activity?type=${type}&participants${people}`,{
-      })
-    .then( response => response.json() )
-    .then( response => addDom(response.activity) )
+    // Call the boredFetch function with the user input values.
+    boredFetch(type);
+});
+
+function addDom(info,info2,info3) {
+    // Display the activity info on the page.
+    answer.innerHTML = JSON.stringify(info);
+    let price = JSON.stringify(info2);
+    answer3.innerHTML = `People: ${JSON.stringify(info3)}`;
+    if (price == 0){
+        answer2.innerHTML = "Price: FREE"
+    } else if (price > 0 && price < 0.1){
+        answer2.innerHTML = "Price: Dirt Cheap";
+    } else if (price >= 0.1 && price < 0.2) {
+        answer2.innerHTML = "Price: Cheap";
+    } else if (price >= 0.2 && price < 0.3) {
+        answer2.innerHTML = "Price: Innexpensive";
+    }else if (price >= 0.3 && price < 0.4) {
+        answer2.innerHTML = "Price: Fair";
+    }else if (price >= 0.4 && price < 0.5) {
+        answer2.innerHTML = "Price: Middle of the Range";
+    }else if (price >= 0.5 && price < 0.6) {
+        answer2.innerHTML = "Price: Getting Pricey";
+    }else if (price >= 0.6 && price < 0.7) {
+        answer2.innerHTML = "Price: Kinda Expensive";
+    }else if (price >= 0.7 && price < 0.8) {
+        answer2.innerHTML = "Price: Too Steep";
+    }else if (price >= 0.8 && price < 0.9) {
+        answer2.innerHTML = "Price: Epensive";
+    }else if (price >= 0.9 && price < 1) {
+        answer2.innerHTML = "Price: Fit for a King";
+    }
+  
 }
 
-function addDom(info){
-    document.getElementById(`answer`).innerHTML = JSON.stringify(info);
+
+function boredFetch( type) {
+    const apiUrl = `http://www.boredapi.com/api/activity?type=${type}`
+    
+
+    fetch(apiUrl)
+    .then((response) => response.json())
+    .then((response) => addDom(response.activity,response.price,response.participants))
+    .catch((err) => console.error(`error:`,err));
+
 }
-boredFetch(people,type);
-})
-
-
-
-
 
